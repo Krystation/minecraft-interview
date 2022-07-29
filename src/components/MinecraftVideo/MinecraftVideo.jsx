1ad29343c51bf19874626a19695b52f69858bec5
mrbeiley <mbeiley2@gmail.com>
2022-07-29 08:51:05 -0700

    Use camel case on HTML attributes.

--- src/components/MinecraftVideo/MinecraftVideo.jsx
+++ src/components/MinecraftVideo/MinecraftVideo.jsx
@@ -5,7 +5,7 @@ const MinecraftVideo = () => {
 	return(
 		<div style={Style} className="container minecraft-video">
 			<h3 className="font-color-sky">ARIZONA MINECRAFT EXPERIENCE</h3>
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/a7GITgqwDVg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/a7GITgqwDVg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 		</div>
 	)
 }
