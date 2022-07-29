0742b3c6a76c864eed86d0a028840243a619f1eb
mrbeiley <mbeiley2@gmail.com>
2022-07-29 08:46:12 -0700

    Check if HowTos is an array before mapping.

--- src/components/HowToPlayWrap/HowToPlayWrap.jsx
+++ src/components/HowToPlayWrap/HowToPlayWrap.jsx
@@ -9,12 +9,12 @@ const HowToPlayWrap = props => {
 		<h4>YOU'LL NEED:</h4>
 			<hr />
             <div classname="how-to-list">
-				{HowTos.map(howTo => (
+				{Array.isArray(HowTos) ? HowTos.map(howTo => (
 					<div key={howTo.id} classname="how-to-item">
 						<img src={require(`../../img/how-to/how-to-${howTo.id+1}.svg`)} alt={howTo.alt} classname="how-to-item-img" />
 						{howTo.text}
 					</div>
-				))}
+				)) : null}
 			</div>
 			<hr />
 			<p classname="email-text">An email will be sent out to those who have registered, with instructions on how to enter the Minecraft-created University of Arizona. If you’re experiencing connection issues, <a href="https://help.minecraft.net/hc/en-us/articles/360035130871-Java-Edition-Realms-connection-issues" rel="noopener">click here</a> for help.</p>
